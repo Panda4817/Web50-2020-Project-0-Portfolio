36e0f5b3d0574235419043c027b08954b4b8a76c
Kanta Munton <kanta.munton@gmail.com>
2020-06-25 22:25:53 +0100

    fixed slow smooth scroll

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,45 +1,42 @@
 
 
 AOS.init();
+if ($('.owl-carousel') ){
+    var welcomeSlide = $('.hero-slides');
 
-$(document).ready(function(){
-    if ($('.owl-carousel') ){
-        var welcomeSlide = $('.hero-slides');
-    
-        welcomeSlide.owlCarousel({
-            items: 1,
-            margin: 0,
-            loop: true,
-            nav: false,
-            dots: false,
-            autoplay: true,
-            autoplayTimeout: 6000,
-            smartSpeed: 1000
+    welcomeSlide.owlCarousel({
+        items: 1,
+        margin: 0,
+        loop: true,
+        nav: false,
+        dots: false,
+        autoplay: true,
+        autoplayTimeout: 6000,
+        smartSpeed: 1000
+    });
+
+    welcomeSlide.on('translate.owl.carousel', function () {
+        var slideLayer = $("[data-animation]");
+        slideLayer.each(function () {
+            var anim_name = $(this).data('animation');
+            $(this).removeClass('animate__' + anim_name).css('opacity', '0');
         });
-    
-        welcomeSlide.on('translate.owl.carousel', function () {
-            var slideLayer = $("[data-animation]");
-            slideLayer.each(function () {
-                var anim_name = $(this).data('animation');
-                $(this).removeClass('animate__' + anim_name).css('opacity', '0');
-            });
+    });
+
+    welcomeSlide.on('translated.owl.carousel', function () {
+        var slideLayer = welcomeSlide.find('.owl-item.active').find("[data-animation]");
+        slideLayer.each(function () {
+            var anim_name = $(this).data('animation');
+            $(this).addClass('animate__' + anim_name).css('opacity', '1');
         });
+    });
+
+    $("[data-delay]").each(function () {
+        var anim_del = $(this).data('delay');
+        $(this).addClass('animate__delay-'+anim_del);
+    });
+}
     
-        welcomeSlide.on('translated.owl.carousel', function () {
-            var slideLayer = welcomeSlide.find('.owl-item.active').find("[data-animation]");
-            slideLayer.each(function () {
-                var anim_name = $(this).data('animation');
-                $(this).addClass('animate__' + anim_name).css('opacity', '1');
-            });
-        });
-    
-        $("[data-delay]").each(function () {
-            var anim_del = $(this).data('delay');
-            $(this).addClass('animate__delay-'+anim_del);
-        });
-    }
-    
-});
 
 function show(data) {
     var div = data.querySelector('.overlay');
