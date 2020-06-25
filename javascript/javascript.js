6e9bc75df9725e4325e09006ecf0f77a2fc8d151
Kanta Munton <kanta.munton@gmail.com>
2020-06-25 15:29:12 +0100

    fixing bugs

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -2,6 +2,13 @@
 
 AOS.init();
 
+$(document).ready(function(){
+    if ( $(".owl-carousel") != null) {
+        $(".owl-carousel").owlCarousel();
+    }
+    
+});
+
 function show(data) {
     var div = data.querySelector('.overlay');
     div.className = "overlay-show";
@@ -22,4 +29,45 @@ function scrollToTop(data) {
     $('html').animate({
         scrollTop: $("#"+data).offset().top
       }, 1000);
+}
+
+if ($('.owl-carousel') ){
+    var welcomeSlide = $('.hero-slides');
+
+    welcomeSlide.owlCarousel({
+        items: 1,
+        margin: 0,
+        loop: true,
+        nav: false,
+        dots: false,
+        autoplay: true,
+        autoplayTimeout: 5000,
+        smartSpeed: 1000
+    });
+
+    welcomeSlide.on('translate.owl.carousel', function () {
+        var slideLayer = $("[data-animation]");
+        slideLayer.each(function () {
+            var anim_name = $(this).data('animation');
+            $(this).removeClass('animated ' + anim_name).css('opacity', '0');
+        });
+    });
+
+    welcomeSlide.on('translated.owl.carousel', function () {
+        var slideLayer = welcomeSlide.find('.owl-item.active').find("[data-animation]");
+        slideLayer.each(function () {
+            var anim_name = $(this).data('animation');
+            $(this).addClass('animated ' + anim_name).css('opacity', '1');
+        });
+    });
+
+    $("[data-delay]").each(function () {
+        var anim_del = $(this).data('delay');
+        $(this).css('animation-delay', anim_del);
+    });
+
+    $("[data-duration]").each(function () {
+        var anim_dur = $(this).data('duration');
+        $(this).css('animation-duration', anim_dur);
+    });
 }
